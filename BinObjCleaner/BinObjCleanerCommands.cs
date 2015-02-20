93bd3e23152a78d5d4bfa904b886abd7443602bb
Maxim Evtukh <maksevtukh@gmail.com>
2015-02-20 23:13:17 +0500

    Add files to repository

---
+++ BinObjCleaner/BinObjCleanerCommands.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BinObjCleaner
+{
+    public enum BinObjCleanerCommands
+    {
+        DeleteBinObj
+    }
+}
+
---
+++ BinObjCleaner/DeleteBinObjHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using MonoDevelop.Components.Commands;
+using MonoDevelop.Ide;
+using MonoDevelop.Core;
+
+namespace BinObjCleaner
+{
+    public class DeleteBinObjHandler : CommandHandler
+    {
+        protected override void Run ()
+        {
+            var projectOperation = IdeApp.ProjectOperations;
+            var isBuild = projectOperation.IsBuilding (projectOperation.CurrentSelectedSolution);
+            var isRun = projectOperation.IsRunning (projectOperation.CurrentSelectedSolution);
+
+            if (!isBuild && !isRun 
+                && IdeApp.ProjectOperations.CurrentBuildOperation.IsCompleted
+                && IdeApp.ProjectOperations.CurrentRunOperation.IsCompleted)
+            {
+                IdeApp.Workbench.StatusBar.BeginProgress("Deleting /bin and /obj folders");
+
+                var solutionItems = projectOperation.CurrentSelectedSolution.Items;
+
+                foreach (var item in solutionItems) {
+                    var binPath = item.BaseDirectory.FullPath + "/bin";
+                    if (FileService.IsValidPath (binPath) && FileService.IsDirectory (binPath))
+                        FileService.DeleteDirectory (binPath);
+
+                    var objPath = item.BaseDirectory.FullPath + "/obj";
+                    if (FileService.IsValidPath (objPath) && FileService.IsDirectory (objPath))
+                        FileService.DeleteDirectory (objPath);
+                }
+
+                IdeApp.Workbench.StatusBar.EndProgress ();
+                IdeApp.Workbench.StatusBar.ShowMessage ("Deleted success");
+            }
+        }
+
+        protected override void Update (CommandInfo info)
+        {
+            info.Visible = IdeApp.Workspace.IsOpen;
+        }
+    }
+}
+
