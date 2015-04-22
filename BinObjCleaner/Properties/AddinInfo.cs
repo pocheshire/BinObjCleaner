e6f9f1782dd547f900c92f7bf27b056c958a2a74
Maxim Evtukh <maksevtukh@gmail.com>
2015-04-22 12:14:35 +0500

    Перевел на Addin Maker 1.2.1

---
+++ BinObjCleaner/Properties/AddinInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly:Addin (
+    "BinObjCleaner", 
+    Namespace = "BinObjCleaner",
+    Version = "1.0"
+)]
+
+[assembly:AddinName ("BinObjCleaner")]
+[assembly:AddinCategory ("IDE extensions")]
+[assembly:AddinDescription ("BinObjCleaner")]
+[assembly:AddinAuthor ("maksimevtuh")]
+
