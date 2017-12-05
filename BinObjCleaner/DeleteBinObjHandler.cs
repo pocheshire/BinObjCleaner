0644b49ff782e1af2926f93fe7d7ec5bea366865
Max Evtukh <maksevtukh@gmail.com>
2017-12-05 17:58:04 +0300

    Updated for Visual Studio

--- BinObjCleaner/DeleteBinObjHandler.cs
+++ BinObjCleaner/DeleteBinObjHandler.cs
@@ -1,42 +1,55 @@
-﻿using System;
-using MonoDevelop.Components.Commands;
-using MonoDevelop.Ide;
+﻿using MonoDevelop.Components.Commands;
 using MonoDevelop.Core;
+using MonoDevelop.Ide;
 
 namespace BinObjCleaner
 {
     public class DeleteBinObjHandler : CommandHandler
     {
-        protected override void Run ()
+        protected override void Run()
         {
             var projectOperation = IdeApp.ProjectOperations;
-            var isBuild = projectOperation.IsBuilding (projectOperation.CurrentSelectedSolution);
-            var isRun = projectOperation.IsRunning (projectOperation.CurrentSelectedSolution);
+            var isBuild = projectOperation.IsBuilding(projectOperation.CurrentSelectedSolution);
+            var isRun = projectOperation.IsRunning(projectOperation.CurrentSelectedSolution);
 
-            if (!isBuild && !isRun 
+            if (!isBuild && !isRun
                 && IdeApp.ProjectOperations.CurrentBuildOperation.IsCompleted
                 && IdeApp.ProjectOperations.CurrentRunOperation.IsCompleted)
             {
-                IdeApp.Workbench.StatusBar.BeginProgress("Deleting /bin and /obj folders");
+                Gtk.Application.Invoke(delegate
+                {
+                    IdeApp.Workbench.StatusBar.BeginProgress(MonoDevelop.Ide.Gui.Stock.StatusBuild, "Deleting /bin and /obj folders...");
+                });
 
                 var solutionItems = projectOperation.CurrentSelectedSolution.Items;
 
-                foreach (var item in solutionItems) {
+                for (int i = 0; i < solutionItems.Count; i++)
+                {
+                    var item = solutionItems[i];
+
                     var binPath = item.BaseDirectory.FullPath + "/bin";
-                    if (FileService.IsValidPath (binPath) && FileService.IsDirectory (binPath))
-                        FileService.DeleteDirectory (binPath);
+                    if (FileService.IsValidPath(binPath) && FileService.IsDirectory(binPath))
+                        FileService.DeleteDirectory(binPath);
 
                     var objPath = item.BaseDirectory.FullPath + "/obj";
-                    if (FileService.IsValidPath (objPath) && FileService.IsDirectory (objPath))
-                        FileService.DeleteDirectory (objPath);
+                    if (FileService.IsValidPath(objPath) && FileService.IsDirectory(objPath))
+                        FileService.DeleteDirectory(objPath);
+
+                    Gtk.Application.Invoke(delegate
+                    {
+                        IdeApp.Workbench.StatusBar.SetProgressFraction((double)i / (double)solutionItems.Count);
+                    });
                 }
 
-                IdeApp.Workbench.StatusBar.EndProgress ();
-                IdeApp.Workbench.StatusBar.ShowMessage ("Deleted successfully");
+                Gtk.Application.Invoke(delegate
+                {
+                    IdeApp.Workbench.StatusBar.ShowMessage("Deleted successfully");
+                    IdeApp.Workbench.StatusBar.EndProgress();
+                });
             }
         }
 
-        protected override void Update (CommandInfo info)
+        protected override void Update(CommandInfo info)
         {
             info.Visible = IdeApp.Workspace.IsOpen;
         }
